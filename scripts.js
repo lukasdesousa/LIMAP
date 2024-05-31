bdd709730a349cef8108f1a6404b75008f26c334
lukito344 <lokasggg27@gmail.com>
2024-05-31 00:05:47 -0300

    Update

--- scripts.js
+++ scripts.js
@@ -15,10 +15,12 @@ function clique() {
     if (cidade == '') {
 
         document.querySelector('.error').innerHTML = 'Insira uma cidade'
+        document.querySelector('#cidade').style.border = '2px solid red'
 
     } else {
         buscarCidade(cidade)
         document.querySelector('.error').innerHTML = ''
+        document.querySelector('#cidade').style.border = ''
 
     }
 }
@@ -66,12 +68,11 @@ async function pol(cidadeCord, dados) {
 
 function colocarnaTela(poluicao, dados) {
     let components = document.querySelector('.componentes')
-    components.innerHTML = '<h2>COMPONENTES NO AR: </h2><br>' + '<h2>Monóxido de Carbono (CO): </h2>' + '<h2>' + poluicao.list[0].components.co + '</h2>' + 'µg/m³' + '<br>' + '<h2>Dióxido de Nitrogênio (NO): </h2>' + '<h2>' + poluicao.list[0].components.no + '</h2>' + 'µg/m³' + '<br>' + '<h2>Amônia (NH3): </h2>' + '<h2>' + poluicao.list[0].components.nh3 + '</h2>' + 'µg/m³' + '<br>' + '<h2>Ozônio (O3): </h2>' + '<h2>' + poluicao.list[0].components.o3 + '</h2>' + 'µg/m³'
-    document.querySelector('.componentes')
+    components.innerHTML = '<h2>COMPONENTES NO AR: </h2><br>' + '<h2>Monóxido de Carbono (CO): </h2>' + '<h2>' + poluicao.list[0].components.co + '</h2>' + '<h3>µg/m³</h3>' + '<br><br>' + '<h3><a style="color: white; text-shadow: 2p 2px 2px black;" href="https://brasilescola.uol.com.br/quimica/monoxido-carbono.htm" target="_blank">O QUE É MONÓXIDO DE CARBONO?</a></h3>' + '<br>' + '<h2>Dióxido de Nitrogênio (NO): </h2>' + '<h2>' + poluicao.list[0].components.no + '<br>' + '</h2>' + '<h3>µg/m³</h3>' + '<br><br>' + '<h3><a style="color: white;" href="https://brasilescola.uol.com.br/quimica/nitrogenio.htm" target="_blank">O QUE É NITROGÊNIO?</a></h3>' + '<br>' +'<h2>Amônia (NH3):' + '</h2>' + '<h2>' + poluicao.list[0].components.nh3 + '</h2>' + '<h3>µg/m³</h3>' + '<br><br>' + '<h3><a style="color: white;" href="https://brasilescola.uol.com.br/quimica/amonia-nh3.htm" target="_blank">O que é AMÔNIA?</a></h3>' + '<br>' + '<h2>Ozônio (O3): </h2>' + '<h2>' + poluicao.list[0].components.o3 + '</h2>' + '<h3>µg/m³</h3>' + '<br><br>' + '<h3><a style="color: white;" href="https://brasilescola.uol.com.br/quimica/gas-ozonio.htm" target="_blank">O que é OZÔNIO?</a></h3>'
 
 
     if (poluicao.list[0].main.aqi == 1) {
-        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>MUITO BOA. </h2>' + '<h2>"AQI (Air Quality Index) de nível 1" </h2>' + "<h2>Não há uma quantidade de componentes que possam fazer mal.<br>MUITO BOM para grupos sensíveis</h2>"
+        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>MUITO BOA. </h2>' + '<h2>"AQI (Air Quality Index) de nível 1" </h2>' + "<h2>Não há uma quantidade de componentes que possam fazer mal.<br><br>MUITO BOM para grupos sensíveis</h2>"
 
 
         document.querySelector('#resultado').style.display = 'block'
@@ -83,7 +84,7 @@ function colocarnaTela(poluicao, dados) {
 
 
     } else if (poluicao.list[0].main.aqi == 2) {
-        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>BOA. </h2>' + '<h2>"AQI (Air Quality Index) de nível 2"</h2>' + "<h2>Há uma quantidade de componentes relativamente baixa.<br>BOM para grupos sensíveis</h2>"
+        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>BOA. </h2>' + '<h2>"AQI (Air Quality Index) de nível 2"</h2>' + "<h2>Há uma quantidade de componentes relativamente baixa.<br><br>BOM para grupos sensíveis</h2>"
 
         document.querySelector('#resultado').style.display = 'block'
 
@@ -93,7 +94,7 @@ function colocarnaTela(poluicao, dados) {
         components.style.background = 'green'
 
     } else if (poluicao.list[0].main.aqi == 3) {
-        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>MODERADA. </h2>' + '<h2>"AQI (Air Quality Index) de nível 3"</h2>' + "<h2>Há uma pequena quantidade de componentes que possam fazer mal.<br>POUCO RUIM para grupos sensíveis</h2>"
+        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>MODERADA. </h2>' + '<h2>"AQI (Air Quality Index) de nível 3"</h2>' + "<h2>Há uma pequena quantidade de componentes que possam fazer mal.<br><br>POUCO RUIM para grupos sensíveis</h2>"
 
         document.querySelector('#resultado').style.display = 'block'
         document.querySelector('#imagens').innerHTML = '<img src="https://cdn-icons-png.flaticon.com/128/166/166527.png" alt="Emojis">'
@@ -102,7 +103,7 @@ function colocarnaTela(poluicao, dados) {
         components.style.background = 'yellow'
 
     } else if (poluicao.list[0].main.aqi == 4) {
-        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>RUIM. </h2>' + '<h2>"AQI (Air Quality Index) de nível 4"</h2>' + "<h2>Há componentes com quantidades elevadas que possam fazer mal.<br>RUIM para grupos sensíveis</h2>"
+        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>RUIM. </h2>' + '<h2>"AQI (Air Quality Index) de nível 4"</h2>' + "<h2>Há componentes com quantidades elevadas que possam fazer mal.<br><br>RUIM para grupos sensíveis</h2>"
 
         document.querySelector('#resultado').style.display = 'block'
 
@@ -112,7 +113,7 @@ function colocarnaTela(poluicao, dados) {
         components.style.background = 'orange'
 
     } else if (poluicao.list[0].main.aqi == 5) {
-        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>MUITO RUIM. </h2>' + '<h2>"AQI (Air Quality Index) de nível 5"</h2>' + "<h2>Há componentes com quantidades muito elevadas.<br>MUITO RUIM para grupos sensíveis</h2>"
+        document.querySelector('#resultado').innerHTML = '<h2>Qualidade do ar em </h2>' + '<h2>' + dados.name.toUpperCase() + ', ' + dados.sys.country + '.' + '</h2>' + '<br>' + '<br>' + '<h2>MUITO RUIM. </h2>' + '<h2>"AQI (Air Quality Index) de nível 5"</h2>' + "<h2>Há componentes com quantidades muito elevadas.<br><br>MUITO RUIM para grupos sensíveis</h2>"
 
         document.querySelector('#imagens').innerHTML = '<img src="https://www.flaticon.com/br/icone-gratis/enjoado_11269963" alt="Emojis">'
 
